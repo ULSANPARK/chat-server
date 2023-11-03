69c53bae20058d0046057c660dbd38da6276992e
ULSANPARK <149644144+ULSANPARK@users.noreply.github.com>
2023-11-03 13:54:11 +0900

    Add files via upload

---
+++ Controllers/chat.controller.js
@@ -0,0 +1,21 @@
+const Chat = require("../Models/chat")
+const chatController = {}
+
+chatController.savaChat = async(message,user)=>{
+    const newMessage = new Chat({
+        chat:message,
+        user:{
+            id:user._id,
+            name:user.name
+        },
+    
+
+    });
+    await newMessage.save();
+    return newMessage;
+
+    
+
+};
+
+module.exports = chatController
